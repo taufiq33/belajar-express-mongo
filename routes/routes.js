22ddb7d8c91cc257dd445e24aa65af0e606c4f7b
taufiq33 <taufiqhidayat1908@gmail.com>
2022-04-21 15:02:03 +0700

    endpoint single pengguna

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const routers = express.Router();
 
+const { ObjectId } = require('mongodb');
+
 const client = require('../db/connection_mongo');
 client.on('connectionReady', () => {
   console.log('berhasil terkoneksi');
@@ -8,14 +10,14 @@ client.on('connectionReady', () => {
 
 routers.get('/pengguna', async (req, res) => {
   const db = client.db('latihan');
-  const products = await db.collection('pengguna').find().toArray();
+  const pengguna = await db.collection('pengguna').find().toArray();
   const response = {
     success: true,
     message: 'Berhasil mendapatkan data semua pengguna',
-    data: products
+    data: pengguna
   };
 
-  if (products.length < 0) {
+  if (pengguna.length < 0) {
     response.success = false;
     response.message = 'Data Pengguna kosong'
   }
@@ -23,8 +25,32 @@ routers.get('/pengguna', async (req, res) => {
   res.json(response);
 });
 
-routers.get('/pengguna/:id', (req, res) => {
-  res.send(`menampilkan pengguna ber id ${req.params.id}`);
+routers.get('/pengguna/:id', async (req, res) => {
+  const db = client.db('latihan');
+
+  const response = {
+    success: true,
+    message: 'Berhasil mendapatkan 1 data pengguna',
+    data: null
+  };
+
+  try {
+    const pengguna = await db.collection('pengguna').findOne({
+      _id: ObjectId(req.params.id)
+    });
+
+    response.data = pengguna;
+
+    if (!pengguna) {
+      response.success = false;
+      response.message = 'Data Pengguna tidak ditemukan'
+    }
+  } catch (error) {
+    response.success = false;
+    response.message = 'Data Pengguna tidak ditemukan'
+  }
+
+  res.json(response);
 });
 
 routers.post('/pengguna', (req, res) => {
