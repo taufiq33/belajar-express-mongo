226e16c5ccd3faa11bfcd764676a4c1952893dd8
taufiq33 <taufiqhidayat1908@gmail.com>
2022-04-24 17:34:31 +0700

    routes get all produk

--- routes/routesProducts.js
+++ routes/routesProducts.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const routers = express.Router();
 
-routers.get('/home', (req, res) => {
-  res.json({
-    'msg': 'success',
-  })
+const connection = require('../db/connection_mongo');
+const ProductModel = require('../db/models/Products');
+
+const DBConnection = connection.connect();
+
+routers.get('/products', async (req, res) => {
+  const products = await ProductModel.find();
+  if (products.length > 0) {
+    res.send({
+      status: 'success',
+      message: 'daftar produk berhasil ditemukan',
+      data: products,
+    })
+  } else {
+    res.send({
+      status: 'success',
+      message: 'daftar produk kosong',
+      data: [],
+    })
+  }
 })
 
 module.exports = routers;
